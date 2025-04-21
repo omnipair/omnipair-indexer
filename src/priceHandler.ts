00c93c6a6f519992ffd842403b5380c3d63bfa2d
Pileks <jure@amber-it.co>
2025-04-21 22:29:58 +0200

    add v4 dao base mints to price indexing

--- src/priceHandler.ts
+++ src/priceHandler.ts
@@ -1,10 +1,13 @@
 import { db, eq, schema } from "@metadaoproject/indexer-db";
-import { PricesRecord, PricesType } from "@metadaoproject/indexer-db/lib/schema";
+import {
+  PricesRecord,
+  PricesType,
+} from "@metadaoproject/indexer-db/lib/schema";
 import { connection } from "./connection";
 import { log } from "./logger/logger";
 
 const logger = log.child({
-  module: "priceHandler"
+  module: "priceHandler",
 });
 
 interface PriceData {
@@ -15,18 +18,48 @@ interface PriceData {
 
 const baseUrl = "https://api.jup.ag/price/v2?ids=";
 
-export async function updatePrices(): Promise<{message:string, error: Error|undefined}> {
-
+export async function updatePrices(): Promise<{
+  message: string;
+  error: Error | undefined;
+}> {
   try {
     const startTime = performance.now();
     //get all the daos that are not hidden
-    const results = await db.select({
-      baseAcct: schema.daos.baseAcct
-    }).from(schema.daos)
-      .leftJoin(schema.daoDetails, eq(schema.daoDetails.daoId, schema.daos.daoId))
-      .where(eq(schema.daoDetails.isHide, false)).execute();
+    const v3Query = db.$with("v3").as(
+      db
+        .select({
+          baseAcct: schema.daos.baseAcct,
+        })
+        .from(schema.daos)
+        .leftJoin(
+          schema.daoDetails,
+          eq(schema.daoDetails.daoId, schema.daos.daoId)
+        )
+        .where(eq(schema.daoDetails.isHide, false))
+    );
 
-  
+    const v4Query = db.$with("v4").as(
+      db
+        .select({
+          baseAcct: schema.v0_4_daos.tokenMintAcct,
+        })
+        .from(schema.v0_4_daos)
+        .leftJoin(
+          schema.organizations,
+          eq(
+            schema.v0_4_daos.organizationId,
+            schema.organizations.organizationId
+          )
+        )
+        .where(eq(schema.organizations.isHide, false))
+    );
+
+    const results = await db
+      .select()
+      .from(v3Query)
+      .union(db.select().from(v4Query))
+      .execute();
+
     let ids = "";
     for (const res of results) {
       ids += res.baseAcct + ",";
@@ -34,16 +67,19 @@ export async function updatePrices(): Promise<{message:string, error: Error|unde
 
     const url = baseUrl + ids;
     const apiKey = process.env.JUPITER_API_KEY || "";
-    
+
     const response = await fetch(url, {
       headers: {
-        'Content-Type': 'application/json',
-        'x-api-key': apiKey
-      }
+        "Content-Type": "application/json",
+        "x-api-key": apiKey,
+      },
     });
     if (!response.ok) {
       logger.error(`Error fetching prices: ${response.statusText}`);
-      return { message: `Error fetching prices: ${response.statusText}`, error: new Error(response.statusText) };
+      return {
+        message: `Error fetching prices: ${response.statusText}`,
+        error: new Error(response.statusText),
+      };
     }
 
     const data = await response.json();
@@ -52,10 +88,9 @@ export async function updatePrices(): Promise<{message:string, error: Error|unde
     let missingPrices = [];
     let errors = [];
     for (const [tokenId, priceData] of Object.entries(data.data)) {
-    
       if (priceData) {
         const pd = priceData as PriceData;
-      
+
         const newPrice: PricesRecord = {
           marketAcct: pd.id,
           price: pd.price,
@@ -65,7 +100,8 @@ export async function updatePrices(): Promise<{message:string, error: Error|unde
         };
 
         try {
-          await db.insert(schema.prices)
+          await db
+            .insert(schema.prices)
             .values(newPrice)
             .onConflictDoNothing()
             .execute();
@@ -73,29 +109,32 @@ export async function updatePrices(): Promise<{message:string, error: Error|unde
           logger.error(`Error inserting price for ${tokenId}: ${error}`);
           errors.push(`Error inserting price for ${tokenId}: ${error}`);
         }
-
       } else {
         logger.warn(`No price data found for ${tokenId}`);
         missingPrices.push(tokenId);
       }
-    
     }
 
     const endTime = performance.now();
-    const missingPricesMessage = missingPrices.filter(Boolean).join('<br>');
-    const message = `Updated prices in ${(endTime - startTime) / 1000}s missing <br>${missingPricesMessage}`;
+    const missingPricesMessage = missingPrices.filter(Boolean).join("<br>");
+    const message = `Updated prices in ${
+      (endTime - startTime) / 1000
+    }s missing <br>${missingPricesMessage}`;
     logger.info(message);
     let errorMessage = "";
     for (const error of errors) {
-      errorMessage += error + '<br>';
+      errorMessage += error + "<br>";
     }
 
-    return { message: message, error: errorMessage ? new Error(errorMessage) : undefined };
-    
+    return {
+      message: message,
+      error: errorMessage ? new Error(errorMessage) : undefined,
+    };
   } catch (error) {
     logger.error(`Error updating prices: ${error}`);
-    return {message: `Error updating prices: ${error}`, error: error instanceof Error ? error : new Error(String(error))};
+    return {
+      message: `Error updating prices: ${error}`,
+      error: error instanceof Error ? error : new Error(String(error)),
+    };
   }
-
-  
 }

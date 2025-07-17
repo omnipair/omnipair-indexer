d3cf5308c028782c05ada439b4a1ced3d0b7c16c
jrooks7 <jeremy@metadao.fi>
2025-07-17 12:35:57 -0700

    fix: uncommenting fields

--- src/index.ts
+++ src/index.ts
@@ -42,59 +42,56 @@ const healthMap = new Map<string, CronRunResult>();
 
 async function main() {
 
-  //first lets backfill v3
-  // let start = new Date();
-  // let res = await backfillV3()
-  // let end = new Date();
-  // let { message, error } = res;
+  // first lets backfill v3
+  let start = new Date();
+  let res = await backfillV3()
+  let end = new Date();
+  let { message, error } = res;
 
 
-  // healthMap.set("backfillV3", new CronRunResult("backfillV3", message, error, start, end, error ? 1 : 0));
+  healthMap.set("backfillV3", new CronRunResult("backfillV3", message, error, start, end, error ? 1 : 0));
 
 
-  // //now lets do v4
-  // start = new Date();
-  // res = await backfillV4()
-  // end = new Date();
-  // ({ message, error } = res);
-  // let totalPreviousErrors = error ? 1 : 0;
-  // healthMap.set("backfillV4", new CronRunResult("backfillV4", message, error, start, end, error ? 1 : 0));
+  //now lets do v4
+  start = new Date();
+  res = await backfillV4()
+  end = new Date();
+  ({ message, error } = res);
+  let totalPreviousErrors = error ? 1 : 0;
+  healthMap.set("backfillV4", new CronRunResult("backfillV4", message, error, start, end, error ? 1 : 0));
 
-  // //now lets frontfill v4
-  // start = new Date();
-  // res = await gapFillV4()
-  // end = new Date();
-  // ({ message, error } = res);
-  // healthMap.set("gapFillV4", new CronRunResult("gapFillV4", message, error, start, end, error ? 1 : 0));
+  //now lets frontfill v4
+  start = new Date();
+  res = await gapFillV4()
+  end = new Date();
+  ({ message, error } = res);
+  healthMap.set("gapFillV4", new CronRunResult("gapFillV4", message, error, start, end, error ? 1 : 0));
 
-  // // time for v5
-  // start = new Date();
-  // res = await backfillV5()
-  // end = new Date();
-  // ({ message, error } = res);
-  // healthMap.set("backfillV5", new CronRunResult("backfillV5", message, error, start, end, error ? 1 : 0));
+  // time for v5
+  start = new Date();
+  res = await backfillV5()
+  end = new Date();
+  ({ message, error } = res);
+  healthMap.set("backfillV5", new CronRunResult("backfillV5", message, error, start, end, error ? 1 : 0));
 
-  // //now lets frontfill v5
-  // start = new Date();
-  // res = await gapFillV5()
-  // end = new Date();
-  // ({ message, error } = res);
-  // healthMap.set("gapFillV5", new CronRunResult("gapFillV5", message, error, start, end, error ? 1 : 0));
+  //now lets frontfill v5
+  start = new Date();
+  res = await gapFillV5()
+  end = new Date();
+  ({ message, error } = res);
+  healthMap.set("gapFillV5", new CronRunResult("gapFillV5", message, error, start, end, error ? 1 : 0));
 
   //lets start our crons now
-  backfillV5();
-  gapFillV5();
-  
-  
-  // startCron("backfillV3", "*/10 * * * *", backfillV3);
-  // startCron("backfillV4", "*/12 * * * *", backfillV4);
-  // startCron("backfillV5", "*/14 * * * *", backfillV5);
-  // startCron("gapFillV4", "*/16 * * * *", gapFillV4);
-  // startCron("gapFillV5", "*/18 * * * *", gapFillV5);
-  // startCron("priceHandler", "* * * * *", priceHandler);
-  // startCron("snapshotV3", "0 */6 * * *", snapshotV3);
-  // startCron("snapshotV4", "5 */6 * * *", snapshotV4);
-  // startCron("snapshotV5", "10 */6 * * *", snapshotV5);
+
+  startCron("backfillV3", "*/10 * * * *", backfillV3);
+  startCron("backfillV4", "*/12 * * * *", backfillV4);
+  startCron("backfillV5", "*/14 * * * *", backfillV5);
+  startCron("gapFillV4", "*/16 * * * *", gapFillV4);
+  startCron("gapFillV5", "*/18 * * * *", gapFillV5);
+  startCron("priceHandler", "* * * * *", priceHandler);
+  startCron("snapshotV3", "0 */6 * * *", snapshotV3);
+  startCron("snapshotV4", "5 */6 * * *", snapshotV4);
+  startCron("snapshotV5", "10 */6 * * *", snapshotV5);
 
   //start tx log subscription
   subscribeAll();

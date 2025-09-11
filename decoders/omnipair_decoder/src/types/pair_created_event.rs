642f0d41b60e79f73694cb1a1249bb058dc1fb86
Muhammed <mselrakabawi@gmail.com>
2025-09-11 10:04:48 +0400

    chore: cargo fix (remove unnecessary imports)

--- decoders/omnipair_decoder/src/types/pair_created_event.rs
+++ decoders/omnipair_decoder/src/types/pair_created_event.rs
@@ -1,6 +1,6 @@
 
 
-use carbon_core::{CarbonDeserialize, borsh, account_utils::next_account};
+use carbon_core::{CarbonDeserialize, borsh};
 
 
 #[derive(CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash)]
